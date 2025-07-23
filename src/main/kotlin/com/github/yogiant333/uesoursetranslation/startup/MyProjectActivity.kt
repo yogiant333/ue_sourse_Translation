c51067abb9a26d52940c22a4b7a35e3978b181ec
GitHub Action <action@github.com>
2025-07-23 00:59:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/yogiant333/uesoursetranslation/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/yogiant333/uesoursetranslation/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.yogiant333.uesoursetranslation.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
